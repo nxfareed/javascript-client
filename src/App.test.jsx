f1b769096b37c1da8aa6268adcca4779b9aba6fc
Fareed Uz Zaman <fareed.zaman@lapr241.successivetech.com>
2020-03-03 18:09:44 +0530

    feat:Applying properties to Input Feilds

---
+++ src/App.test.jsx
@@ -0,0 +1,7 @@
+
+
+test('renders learn react link', () => {
+  // const { getByText } = render(<App />);
+  // const linkElement = getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+});
