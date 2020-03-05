6469f7b7803d11dcf5d91d97940ac17b70789e06
Fareed Uz Zaman <fareed.zaman@lapr241.successivetech.com>
2020-03-05 18:48:20 +0530

    feat:Applying State

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { getRandomNumber, getNextRoundRobin } from './../../libs/utils/math';
+import PropTypes from 'prop-types';
+import { PUBLIC_IMAGE_FOLDER, DEFAULT_BANNER_IMAGE } from '../../config/constants'
+import Img from './style'
+
+class Slider extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: -1,
+    }
+  }
+
+  componentDidMount() {
+    const { banners, duration, random } = this.props
+    this.id = setInterval(() => {
+      let { counter } = this.state;
+
+      if (random && banners.length)
+        counter = getRandomNumber(banners.length);
+      else if (banners.length)
+        counter = getNextRoundRobin(counter, banners.length)
+
+      this.setState({ counter });
+    }, duration)
+
+  }
+  // componentDidMount() {
+  //   //console.log('inside component did mount');
+  //   const { banners, duration, random } = this.props
+  //   this.id = setInterval(() => {
+  //   //console.log('inside set time interval');
+  //   let { counter } = this.state;
+
+  //   if (random && banners.length)
+  //   counter = getRandomNumber(banners.length);
+  //   else if (banners.length)
+  //   counter = getNextRoundRobin(banners.length, counter);
+
+  //   //console.log('update counter',counter);
+
+  //   this.setState({ counter })
+  //   }, duration)
+
+  //   }
+  componentWillUnmount() {
+    clearInterval(this.id);
+  }
+  render() {
+    const { banners, altText, height, defaultBanner } = this.props;
+    const { counter } = this.state;
+    let imagePath
+    if (counter === -1) {
+      if (defaultBanner)
+        imagePath = `${PUBLIC_IMAGE_FOLDER}banners/${defaultBanner}`;
+      else {
+        imagePath = PUBLIC_IMAGE_FOLDER + DEFAULT_BANNER_IMAGE;
+      }
+    }
+    else
+      imagePath = PUBLIC_IMAGE_FOLDER + banners[counter];
+    return (
+      <Img src={imagePath} alt={altText} height={height} />
+    )
+  }
+}
+Slider.propTypes = {
+  altText: PropTypes.string,
+  banners: PropTypes.arrayOf(PropTypes.string),
+  defaultBanner: PropTypes.string,
+  duration: PropTypes.number,
+  height: PropTypes.number,
+  random: PropTypes.bool
+};
+
+Slider.defaultProps = {
+  altText: 'Default Banner',
+  banners: [],
+  defaultBanner: 'default.png',
+  duration: 1000,
+  height: 200,
+  random: false
+};
+export default Slider;
--- src/pages/TextFieldDemo/TextFieldDemo.jsx
+++ src/pages/TextFieldDemo/TextFieldDemo.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import TextFeild from './../../components/TextField/TextFeild'
 import {Div, P, Err} from '../../components/TextField/style'
+import Slider from '../../components/Slider/Slider';
+import {banners} from './../../config/constants'
 
 const TextFeildDemo = () => (
-
   <Div>
+    <Slider altText='image not load' banners={banners} defaultBanner='default.png' height={200} duration={2000} random={false}/>
+
     <P>This is Disabled Input</P>
     <TextFeild value='Disabled Input' disabled/>
     <P>A valid input</P>
@@ -13,7 +16,6 @@ const TextFeildDemo = () => (
     <TextFeild value='101' />
     <Err>could not be greater than</Err>
   </Div>
-
 );
 
 export default TextFeildDemo;
