2a0e7eec253cb8178ed1e2dcfc53c3872edd9da4
Fareed Uz Zaman <fareed.zaman@lapr241.successivetech.com>
2020-03-18 01:25:04 +0530

    feat: Add NavBar and Login UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Trainee from './pages/Trainee/index';
+import Login from './pages/Login/Login'
 
 
 function App() {
   return (
     <div className="App">
+        {/* Use either of the two defined pages at a time as told in the task.
+        <Login/> */}
         <Trainee/>
     </div>
   );
