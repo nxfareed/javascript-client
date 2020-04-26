fdc97d2c84ef8f0321e04714445f35850a854df0
Fareed Uz Zaman <fareed.zaman@lapr241.successivetech.com>
2020-04-26 07:51:51 +0530

    feat: Resolve Errors

--- src/pages/ChildrenDemo/ChildrenDemo.jsx
+++ src/pages/ChildrenDemo/ChildrenDemo.jsx
@@ -1,66 +1,41 @@
-import React from 'react';
-import { ThemeProvider } from '@material-ui/core';
-import Text from '../../components/Math/Math';
-import theme from '../../theme';
-import { Typography } from '@material-ui/core';
-
+import React from "react";
+import { ThemeProvider } from "@material-ui/core";
+import Math from "../../components/Math/Math";
+import theme from "../../theme";
+import { Typography } from "@material-ui/core";
 
 export default class CalculatorDemo extends React.Component {
   Result() {
     let { result } = this.state;
-    result = '';
+    result = "";
     this.setState({ result });
   }
 
   render() {
     return (
       <>
-        <Text first={7} second={4} operator="+" />
-        <Text first={7} second={3} operator="-" />
-        <Text first={5} second={18} operator="*" />
-        <Text first={7} second={0} operator="/" />
-        <Text first={3} second={5} operator="+">
-          {
-            (first, second, result) => (
-              <p>
-                Sum of
-{' '}
-                {first}
-                {' '}
-                and
-{' '}
-                {second}
-                {' '}
-                is equal to
-{' '}
-                {result}
-                {' '}
-              </p>
-            )
-          }
-        </Text>
         <ThemeProvider theme={theme}>
           <Typography>
-            <Text first={5} second={4} operator="+">
-              {
-                (first, second, result) => (
-                  <p>
-                    Sum of
-{' '}
-                    {first}
-                    {' '}
-                    and
-{' '}
-                    {second}
-                    {' '}
-                    is equal to
-{' '}
-                    {result}
-                    {' '}
-                  </p>
-                )
-              }
-            </Text>
+            <Math first={7} second={4} operator="+" />
+            <Math first={7} second={3} operator="-" />
+            <Math first={5} second={18} operator="*" />
+            <Math first={0} second={0} operator="/" />
+            <Math first={3} second={5} operator="+">
+              {(first, second, result) => (
+                <p>
+                  Sum of {first} and {second} is equal to {result}{" "}
+                </p>
+              )}
+            </Math>
+            {/* <ThemeProvider theme={theme}>
+          <Typography> */}
+            <Math first={5} second={4} operator="+">
+              {(first, second, result) => (
+                <p>
+                  Sum of {first} and {second} is equal to {result}{" "}
+                </p>
+              )}
+            </Math>
           </Typography>
         </ThemeProvider>
       </>
