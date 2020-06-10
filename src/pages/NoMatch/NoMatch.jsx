296e99d8f875cf43dbc52831ea1bce7eb860fe65
Fareed Uz Zaman <fareed.zaman@lapr241.successivetech.com>
2020-06-10 14:32:08 +0530

    feat: Resolve Conflicts

--- src/pages/NoMatch/NoMatch.jsx
+++ src/pages/NoMatch/NoMatch.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import React from "react";
+// import CssBaseline from '@material-ui/core/CssBaseline';
+// import Typography from '@material-ui/core/Typography';
+import {
+  makeStyles,
+  CssBaseline,
+  Container,
+  Typography,
+} from "@material-ui/core";
+// import Container from '@material-ui/core/Container';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    flexDirection: 'column',
-    minHeight: '100vh',
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100vh",
   },
   main: {
     marginTop: theme.spacing(8),
--- src/pages/Trainee/TraineeDetail.jsx
+++ src/pages/Trainee/TraineeDetail.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Typography from "@material-ui/core/Typography";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  makeStyles,
+} from "@material-ui/core";
 import moment from "moment";
 import { useParams, useHistory } from "react-router-dom";
 import Button from "../../components/button/button";
